8f25e6d6a93df32b3a8bb3ad7799b7fd91cb0028
juanluisT <juan.it.cto@gmail.com>
2022-09-08 11:03:16 -0700

    tarea practices #2,#4 y #6

---
+++ Unit1/practices/practice3Scala.scala
@@ -0,0 +1,26 @@
+
+
+//2. Desarrollar un algoritmo en scala que me diga si un numero es primo
+var num = 5
+if (num % 2 == 0) {
+var primo =  printf("No es número primo")
+} else {
+var primo = printf("Si es número primo")
+}
+/******************************************************/
+
+
+
+//4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slilce para extraer la
+//   secuencia "Luke"
+val Luk = "Hola Luke yo soy tu padre!"
+//Luk.slice (5,9)
+/******************************************************/
+
+
+
+//6. Dada la tupla (2,4,5,1,2,3,3.1416,23) regresa el numero 3.1416 
+val my_tup = (2,4,5,1,2,3,3.1416,23)
+//my_tup._7
+
+/******************************************************/
