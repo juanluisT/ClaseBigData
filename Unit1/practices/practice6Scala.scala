f0fa5a413e7cf95e3681b15b7f99c42b3d384a63
juanluisT <juan.it.cto@gmail.com>
2022-09-21 12:36:36 -0700

    adding practice #6 actvities

---
+++ Unit1/practices/practice6Scala.scala
@@ -0,0 +1,37 @@
+//Algoritmo 3 Versión iterativa
+//(Complejidad {\displaystyle O(n)\,}O(n)\,)
+
+  //def fibonacci(n: Int): Int = {
+  //  if ( n == 0 ) 0
+  //  else if ( n == 1 ) 1
+  //  else fibonacci( n-1 ) + fibonacci( n-2 )
+  //}
+  //fibonacci(10)
+/*********************************************/
+
+//Algoritmo 4 Versión iterativa
+//2 variables (Complejidad {\displaystyle O(n)\,}O(n)\,)
+//def fib(n: Int): Int = {
+//var a = 0
+//var b = 1
+//for(a <- 0 to n){
+  // b = b + b 
+  // a = b + a
+//return b
+//}
+//}
+
+//fib(10)
+
+
+//Algoritmo 5 Versión iterativa vector (Complejidad {\displaystyle O(n)\,}O(n)\,)
+def fib(n: Int): Int = {
+if ( n <2 )
+  return n
+else if()
+
+for(a <- 0 to n+1){
+}
+}
+
+fib(10)
